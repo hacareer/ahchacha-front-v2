a91dea2cf106b8e4def14bb91c77fc7a1c152b18
nant0313 <nant0313@gmail.com>
2021-11-28 01:56:05 +0900

    Add folder: android

---
+++ android/app/src/main/java/com/pathfinder/ahchacha/MainActivity.java
@@ -0,0 +1,27 @@
+package com.pathfinder.ahchacha;
+
+import com.facebook.react.ReactActivity;
+
+// react-native-bootsplash
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash; //react-native-bootsplash
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ahchacha";
+  }
+
+  // Added
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null); // with react-native-screens
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // display the generated bootsplash.xml drawable over
+                                                                 // our MainActivity
+  }
+}
