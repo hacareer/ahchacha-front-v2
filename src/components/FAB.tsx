47d15796a1f52260beb82f7e5c49595fb1d0c8e2
nant0313 <nant0313@gmail.com>
2021-11-28 02:05:54 +0900

    Add component: floating-action-button

---
+++ src/components/FAB.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {GestureResponderEvent, Pressable, StyleSheet} from 'react-native';
+import {SvgXml} from 'react-native-svg';
+import Text from './Atoms/Text';
+import Alarm from '../../assets/images/bottom_tab/Calendar.svg';
+
+interface IFABProps {
+  onPress?: (e: GestureResponderEvent) => void;
+  text: string;
+}
+export default function FAB(props: IFABProps) {
+  const {onPress = () => {}, text} = props;
+  const styles = StyleSheet.create({
+    wrap: {
+      width: 64,
+      height: 64,
+      borderRadius: 5000,
+      backgroundColor: '#FFC715',
+      bottom: 25,
+      right: 20,
+      zIndex: 102,
+      position: 'absolute',
+      justifyContent: 'center',
+      alignItems: 'center',
+      elevation: 3,
+    },
+  });
+  return (
+    <Pressable style={styles.wrap} onPress={onPress}>
+      <SvgXml xml={Alarm} color="white" stroke="white" width={24} height={22}/>
+    </Pressable>
+  );
+}
