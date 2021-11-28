1450e98244691ba7f74528cafb7c1bb979635179
nant0313 <nant0313@gmail.com>
2021-11-28 09:36:03 +0900

    Add push notification receiver

--- index.js
+++ index.js
@@ -5,5 +5,23 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import notifee, {
+  AndroidImportance,
+  AndroidBigPictureStyle,
+  AndroidStyle,
+} from '@notifee/react-native';
+import messaging from '@react-native-firebase/messaging';
 
+notifee.createChannelGroups([{id: 'group', name: '일반'}]);
+notifee.createChannel({
+  importance: AndroidImportance.HIGH,
+  id: 'channel',
+  name: '기타',
+  description: '기타 알림입니다.',
+  groupId: 'group',
+});
+function messageReceiveHandler(message) {
+  console.log(message);
+}
+messaging().onMessage(messageReceiveHandler);
 AppRegistry.registerComponent(appName, () => App);
