f9d07bba32b03c45ffe96899c1ecfbaccc87f777
nant0313 <nant0313@gmail.com>
2021-11-28 09:38:10 +0900

    Add bootsplash svg->xml

--- android/app/src/main/java/com/pathfinder/ahchacha/MainActivity.java
+++ android/app/src/main/java/com/pathfinder/ahchacha/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null); // with react-native-screens
-    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // display the generated bootsplash.xml drawable over
+    RNBootSplash.init(R.drawable.ic_bootsplash, MainActivity.this); // display the generated bootsplash.xml drawable over
                                                                  // our MainActivity
   }
 }
