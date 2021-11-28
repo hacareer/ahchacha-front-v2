c4a147227fdb1b98fb0f7a9fc738d9b9b86feb14
nant0313 <nant0313@gmail.com>
2021-11-28 12:47:27 +0900

    Change logic: hide splash on login

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -9,18 +9,25 @@ import UtilNavigator from './UtilNavigator';
 import SchoolNavigator from './SchoolNavigator';
 import {useSelector} from 'react-redux';
 import {IReduxState} from '../redux/types';
+import RNBootSplash from 'react-native-bootsplash';
 
 const Stack = createStackNavigator<RootStackParamList>();
 export default function RootNavigator() {
   const userInfo = useSelector((state: IReduxState) => state.userInfo);
   const screenOptions = {headerShown: false};
+  React.useEffect(() => {
+    if (userInfo) {
+      // hide boot splash
+      RNBootSplash.hide({fade: true}); // fade
+    }
+  }, [userInfo]);
   return (
     <Stack.Navigator screenOptions={screenOptions}>
       {!userInfo && (
         <Stack.Screen name="Register" component={RegisterNavigator} />
-        )}
+      )}
       <Stack.Screen name="BottomTab" component={BottomTabNavigator} />
-        <Stack.Screen name="Clinic" component={ClinicNavigator} />
+      <Stack.Screen name="Clinic" component={ClinicNavigator} />
       <Stack.Screen name="Calendar" component={CalendarNavigator} />
       <Stack.Screen name="Util" component={UtilNavigator} />
       <Stack.Screen name="School" component={SchoolNavigator} />
